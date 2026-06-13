2b2746f94626316069d5e9ebba9c0acbf9d1c2fd
irfannesya <141003106+irfannesya@users.noreply.github.com>
2026-06-13 15:41:04 +0700

    config3

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
 const orderRoutes = require("./routes/orderRoutes")
 app.use("/order", orderRoutes)
+
+app.listen(this.prototype, () => {
+    console.log(`server berjalan di port ${PORT}`);
+
+})
 
 module.exports = app
